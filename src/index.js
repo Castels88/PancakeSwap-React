3670d6a3c605eeccac63598e5618d8b2b6bdd7ed
otnd <davide.morrone01@gmail.com>
2022-09-14 15:55:43 +0200

    Pancakeswap in React - Repo created

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css';
+import './SCSS-Modules/connect_wallet.css'
+import './SCSS-Modules/settings.css'
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
