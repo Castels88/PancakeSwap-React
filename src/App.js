094f1f0b40064ace72fa010f0578530d91340df1
Federico <muneramulas@hotmail.com>
2022-10-04 12:26:06 +0200

    reset

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
+import './style.css'
 
-
-function App() {
+export default function App() {
   return (
-    
+    <div>
+    </div>
   );
 }
 
-export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Style.css';
-import './SCSS-Modules/connect_wallet.css'
-import './SCSS-Modules/settings.css'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
