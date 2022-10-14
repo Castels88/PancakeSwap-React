610ab1dc2cdf8243af4e82e2b3de4236928875fd
Federico <muneramulas@hotmail.com>
2022-10-14 15:35:24 +0200

    fix

--- src/React-Modules/MoonMillions/Moon.js
+++ src/React-Modules/MoonMillions/Moon.js
@@ -23,40 +23,6 @@ export function Moon() {
 
   return (
     <div className="moon_container">
-      <div className="moon_backgroundimg">
-        {/* <img src={output} alt="" /> */}
-        <svg
-          viewBox="0 0 1660 339"
-          class="sc-8a800401-0 fGhPpn slide-svg-dark"
-          width="100%"
-          color="text"
-          xmlns="http://www.w3.org/2000/svg"
-          src={output}
-        >
-          <path
-            d="M804 166.523C520.5 166.523 267.5 290.022 0 304V338.5H1660V0C1358.83 0 1104 166.523 804 166.523Z"
-            class=""
-          ></path>
-          <defs>
-            <linearGradient
-              id="paint0_linear_dark"
-              x1="830"
-              y1="83.5"
-              x2="830"
-              y2="338.5"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stop-color="#08060B" stop-opacity="0.2"></stop>
-              <stop
-                offset="0.545554"
-                stop-color="#08060B"
-                stop-opacity="0.5"
-              ></stop>
-              <stop offset="1" stop-color="#08060B"></stop>
-            </linearGradient>
-          </defs>
-        </svg>
-      </div>
       <div className="moon_top">
         <>
           {carouselOne ? (
@@ -113,6 +79,42 @@ export function Moon() {
             style={{ backgroundColor: carouselTwo ? "white" : "gray" }}
           ></button>
         </div>
+      </div>
+
+      {/* SVG background between sections */}
+      <div className="moon_backgroundimg">
+        {/* <img src={output} alt="" /> */}
+        <svg
+          viewBox="0 0 1660 339"
+          class="sc-8a800401-0 fGhPpn slide-svg-dark"
+          width="100%"
+          color="text"
+          xmlns="http://www.w3.org/2000/svg"
+          src={output}
+        >
+          <path
+            d="M804 166.523C520.5 166.523 267.5 290.022 0 304V338.5H1660V0C1358.83 0 1104 166.523 804 166.523Z"
+            class=""
+          ></path>
+          <defs>
+            <linearGradient
+              id="paint0_linear_dark"
+              x1="830"
+              y1="83.5"
+              x2="830"
+              y2="338.5"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#08060B" stop-opacity="0.2"></stop>
+              <stop
+                offset="0.545554"
+                stop-color="#08060B"
+                stop-opacity="0.5"
+              ></stop>
+              <stop offset="1" stop-color="#08060B"></stop>
+            </linearGradient>
+          </defs>
+        </svg>
       </div>
 
       <div className="moon_bottom">
