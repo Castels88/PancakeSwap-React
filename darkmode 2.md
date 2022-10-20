2507af5712bdffd57b775780c59a799065c8e84e
l7g <laurent.gagne@icloud.com>
2022-10-20 10:31:34 +0200

    fix: minor bug

--- darkmode 2.md
+++
@@ -1,3 +0,0 @@
-## Local Storage
-
-- Per poter funzionare dovete installare il modulo Local Storage [`npm i use-local-storage`]
--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import { Cake } from "./React-Modules/Cake/Cake";
 import { DarkMode } from "./React-Modules/Common-Components/Dark Mode/DarkMode.js";
-import { DarkTest } from "./React-Modules/WinMillions/DarkTest";
-import { WinMillions } from "./React-Modules/WinMillions/WinMillions";
 
 function App() {
   return (
