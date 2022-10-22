048a03e6654b3775d748b905ae9eca4739b7acd8
maxuel99 <massimocaser@gmail.com>
2022-10-22 15:49:09 +0200

    fix class svg section Trade

--- src/React-Modules/EarnPassiveIncome/EarnPassiveIncome.js
+++ src/React-Modules/EarnPassiveIncome/EarnPassiveIncome.js
@@ -7,15 +7,15 @@ import { TopFarm } from './TopFarm'
 export function EarnPassiveIncome(props) {
   return (
     <div className="Earn_p_i_big_contaniner">
-      <svg
-        viewBox="0 0 1660 48"
-        preserveAspectRatio="none"
-        color="text"
-        xmlns="http://www.w3.org/2000/svg"
-        class="sc-8a800401-0 fGhPpn"
-      >
-        <path d="M-346 48C174.985 46.1887 977.744 15.2453 1314 0H-346V48Z"></path>
-      </svg>
+        <svg
+          viewBox="0 0 1660 48"
+          preserveAspectRatio="none"
+          color="text"
+          xmlns="http://www.w3.org/2000/svg"
+          className="sc-8a800401-0 fGhPpn svg-divisor"
+        >
+          <path d="M-346 48C174.985 46.1887 977.744 15.2453 1314 0H-346V48Z"></path>
+        </svg>
       <div className="Earn_p_i_top_container">
         <div className="Earn_p_i_medium_container"></div>
         <div className="Earn_p_i_central_container">
