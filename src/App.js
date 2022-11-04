6f7b41935e2050a521141fda19f0e34772ee6605
Federico <muneramulas@hotmail.com>
2022-11-04 19:39:48 +0100

    add feature NFT assets

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import { DarkMode } from "./React-Modules/Dark Mode/DarkMode";
 import { Moon } from "./React-Modules/MoonMillions/Moon";
 import { UsedBy } from "./React-Modules/MoonMillions/UsedBy";
+import { Nft } from "./React-Modules/nft/Nft";
 import "./style.css";
 
 export default function App() {
   return (
     <>
-      <DarkMode />
+      {/* <DarkMode /> */}
       {/* <Moon /> */}
       {/* <UsedBy /> */}
+      <Nft />
     </>
   );
 }
