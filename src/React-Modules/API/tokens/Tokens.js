17438be7980a3128c56c8667056b0f559aa51096
[elio] <[castels@hotmail.it]>
2022-11-07 11:15:27 +0000

    fixed swap between table and chart

--- src/React-Modules/API/tokens/Tokens.js
+++ src/React-Modules/API/tokens/Tokens.js
@@ -1,65 +1,69 @@
-import React from "react";
-import axios from "axios";
-import { useEffect } from "react";
-import { useState } from "react";
-import { Chart } from "react-google-charts";
-import { getChartData, getRndInteger } from "./script.js";
+import React from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { Chart } from 'react-google-charts'
+import { getChartData, getRndInteger } from './script.js'
 const CoinChart = () => {
   //States to get apis response
-  const [fetchData, setFetchData] = useState();
-  const [chartData, setChartData] = useState();
+  const [fetchData, setFetchData] = useState()
+  const [chartData, setChartData] = useState()
   //States to store and create points and values
-  const [value, setValue] = useState({});
+  const [value, setValue] = useState({})
   //States for chart options
-  const [data, setData] = useState();
-  const [options, setOptions] = useState();
+  const [data, setData] = useState()
+  const [options, setOptions] = useState()
 
   //API url
   const url =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
 
   //Api call
   useEffect(() => {
-    axios.get(url).then((response) => {
-      setFetchData(response.data);
-      return response.data
-    }).then((res) => {
-      let defaultChartData = res.filter((obj) => obj.id === 'bitcoin')
-      .find((el) => el);
-      setChartData(defaultChartData)
-      setValue({
-        high: defaultChartData.high_24h,
-        current: defaultChartData.current_price,
-        low: defaultChartData.low_24h,
-      });
-    });
-  }, []);
+    axios
+      .get(url)
+      .then((response) => {
+        setFetchData(response.data)
+        return response.data
+      })
+      .then((res) => {
+        let defaultChartData = res
+          .filter((obj) => obj.id === 'bitcoin')
+          .find((el) => el)
+        setChartData(defaultChartData)
+        setValue({
+          high: defaultChartData.high_24h,
+          current: defaultChartData.current_price,
+          low: defaultChartData.low_24h,
+        })
+      })
+  }, [])
 
   function changeHandler(event) {
     let currCoinData = fetchData
       .filter((obj) => obj.id === event.target.value)
-      .find((el) => el);
-    setChartData(currCoinData);
+      .find((el) => el)
+    setChartData(currCoinData)
     setValue({
       high: currCoinData.high_24h,
       current: currCoinData.current_price,
       low: currCoinData.low_24h,
-    });
+    })
   }
 
   useEffect(() => {
-    let points = [];
+    let points = []
     setTimeout(() => {
       for (let i = 0; i <= 9; i++) {
-        let rndPoint = getRndInteger(value.low, value.high);
-        points.push(rndPoint);
+        let rndPoint = getRndInteger(value.low, value.high)
+        points.push(rndPoint)
       }
-      getChartData(points, chartData, value.current, setData, setOptions);
-    }, 100);
-  }, [value]);
+      getChartData(points, chartData, value.current, setData, setOptions)
+    }, 100)
+  }, [value])
 
   return (
-    <div className="" style={{height:'100%' , width:'100%'}}>
+    <div className="" style={{ height: '100%', width: '100%' }}>
       <select name="coins" id="coins" onChange={changeHandler}>
         {fetchData &&
           fetchData.map((coin, index) => (
@@ -68,6 +72,7 @@ const CoinChart = () => {
             </option>
           ))}
       </select>
+
       {chartData && (
         <Chart
           chartType="AreaChart"
@@ -78,7 +83,7 @@ const CoinChart = () => {
         />
       )}
     </div>
-  );
-};
+  )
+}
 
-export default CoinChart;
+export default CoinChart
