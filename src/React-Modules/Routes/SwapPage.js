c36fe8425ffb29e36fd79ba8632f635dafd9b869
[elio] <[castels@hotmail.it]>
2022-11-09 17:44:51 +0000

    fixed dark-mode swap page

--- src/React-Modules/Routes/SwapPage.js
+++ src/React-Modules/Routes/SwapPage.js
@@ -9,6 +9,7 @@ import CoinChart from '../API/tokens/Tokens.js'
 import Converter from '../API/converter/Converter.js'
 import { useState } from 'react'
 import { ChartFromMobile } from './Componets-from-Route/ChartForMobile'
+import useLocalStorage from 'use-local-storage'
 
 export function SwapPage() {
   const [chartIsShow, setchartIsShow] = useState(true)
@@ -25,10 +26,20 @@ export function SwapPage() {
     setVisible(!visible)
     setVisibleTwo(!visibleTwo)
   }
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+  const [theme, setTheme] = useLocalStorage(
+    'theme',
+    defaultDark ? 'dark' : 'light',
+  )
+  window.dispatchEvent(new Event('storage'))
 
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light'
+    setTheme(newTheme)
+  }
   return (
-    <>
-      <Navbar />
+    <div className="darkswitch" data-theme={theme}>
+      <Navbar switchTheme={switchTheme} />
 
       <div className="swap_container">
         <div className="swap_link_container">
@@ -106,7 +117,7 @@ export function SwapPage() {
                     <div className="swap_group_icon">
                       <h2 className="swap_h2_text">Swap</h2>
                       <div className="swap_componet">
-                        <SettingsComponent />
+                        <SettingsComponent switchTheme={switchTheme} />
                       </div>
                       <div className="swap_rec_transaction">
                         <svg
@@ -247,7 +258,7 @@ export function SwapPage() {
                             alt="img-HAY"
                             style={{ width: '24px', marginRight: '5px' }}
                           />
-                          <p class="swap-text">HAY</p>
+                          <p class="swap-text-crypto">HAY</p>
                           <svg
                             class="swap_SVG_copy"
                             viewBox="0 0 24 24"
@@ -290,7 +301,7 @@ export function SwapPage() {
                             alt="img-BUSD"
                             style={{ width: '24px', marginRight: '5px' }}
                           />
-                          <p class="swap-text">BUSD</p>
+                          <p class="swap-text-crypto">BUSD</p>
                           <svg
                             class="swap_SVG_copy"
                             viewBox="0 0 24 24"
@@ -331,27 +342,7 @@ export function SwapPage() {
                       </div>
                     </div>
                   </div>
-                  {/* {visible ? (
-                    <div>
-                      <div>
-                        <div className="container-svg-stableSwap">
-                          <svg
-                            viewBox="0 0 24 24"
-                            color="warning"
-                            width="24px"
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="sc-4ba21b47-0 joVAFt"
-                          >
-                            <path d="M4.47 20.9999H19.53C21.07 20.9999 22.03 19.3299 21.26 17.9999L13.73 4.98993C12.96 3.65993 11.04 3.65993 10.27 4.98993L2.74 17.9999C1.97 19.3299 2.93 20.9999 4.47 20.9999ZM12 13.9999C11.45 13.9999 11 13.5499 11 12.9999V10.9999C11 10.4499 11.45 9.99993 12 9.99993C12.55 9.99993 13 10.4499 13 10.9999V12.9999C13 13.5499 12.55 13.9999 12 13.9999ZM13 17.9999H11V15.9999H13V17.9999Z"></path>
-                          </svg>
-                        </div>
-                        <div>
-                          Trade stablecoins in StableSwap with lower slippage
-                          and trading fees!
-                        </div>
-                      </div>
-                    </div>
-                  ) : null} */}
+
                   <div className="swap_conteiner_component">
                     <ButtonConnectWallet />
                   </div>
@@ -410,7 +401,7 @@ export function SwapPage() {
         </div>
       </div>
       {chartMobile ? <ChartFromMobile /> : null}
-      <Footer />
-    </>
+      <Footer switchTheme={switchTheme} />
+    </div>
   )
 }
